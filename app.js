8f8e54111e6875b9382521c9c9e871f9f1ac1293
Qing Zhou <qingzhou@Qings-MacBook-Pro.local>
2022-04-16 11:46:23 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = mongoose.model("User", userSchema);
+
+
+app.listen(3000, function(){
+  console.log("Server is up and running")
+});
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+
+  newUser.save(function(err){
+    if (!err) {
+      res.render("secrets");
+    }
+  })
+})
+
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({username: username}, function(err,result){
+    if (err) {
+      console.log(err)
+    }
+    else {
+      if (result) {
+        if (result.password === password) {
+          res.render("secrets");
+        }
+        else {
+          res.send("password doesn't match");
+        }
+
+      }
+      else {
+        res.send("username not found");
+      }
+    }
+  })
+});
